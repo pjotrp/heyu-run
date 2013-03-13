9b187ba64c279096855aaf91083103b116bff198
Pjotr Prins <pjotr.public01@thebird.nl>
2013-03-13 18:28:57 +0100

    Load tests on demand

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 js=/opt/js-1.6.20070208/bin/js
-coffee -c bin/heyu-run.coffee lib/*.coffee
+coffee -c bin/heyu-run.coffee lib/*.coffee test/*.coffee
 if [ $? -eq 0 ]; then 
   $js bin/heyu-run.js --test
 fi
